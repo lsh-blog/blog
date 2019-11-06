7b438fa08ae06deb9a0bbb9648667bd7514a3542
猿计划 <oncwnuIE8bl6OvQXfESYL-d0Nuww@git.weixin.qq.com>
2019-11-06 18:55:24 +0800

    发布到github

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -51,20 +51,6 @@
     <!--live2d看板娘-->
     <Live2D v-if="isPc"></Live2D>
 
-    <script v-once v-pre>
-      (function () {
-        var bp = document.createElement('script');
-        var curProtocol = window.location.protocol.split(':')[0];
-        if (curProtocol === 'https') {
-          bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-        } else {
-          bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-        }
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(bp, s);
-      })();
-    </script>
-
   </div>
 </template>
 
